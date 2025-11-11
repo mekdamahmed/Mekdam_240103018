f1d5301695b763c5df9049704b4c7d3f5b9ffbd2
mekdamahmed <mekdammurad2006@gmail.com>
2025-11-12 01:27:11 +0200

    Create Boy or Girl9.cpp

---
+++ Codeforce/Boy or Girl9.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+string username;
+cin >> username;
+int count;
+
+for(char i : username){
+    if (username.find)
+}
+
+if (count%2==0){
+    cout << "CHAT WITH HER!";
+}
+else{
+    cout << "IGNORE HIM!";
+}
+}
