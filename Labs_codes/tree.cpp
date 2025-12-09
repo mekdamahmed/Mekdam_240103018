50b026c581e6bef37d9ff1b5ef428854764e1669
mekdamahmed <mekdammurad2006@gmail.com>
2025-12-09 22:41:03 +0200

    lab code

---
+++ Labs_codes/tree.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std; 
+
+    class Node {
+        public:
+        int data;
+        Node* left, *right;
+
+        Node(int value){
+            data = value;
+            left= right = NULL;
+        }
+        
+    };
+
+    class BinarySearchTree{
+        Node* root;
+        BinarySearchTree(){
+            root=NULL;
+        }
+        Node* insert(Node* r, int item){
+            if(r==NULL){
+0                Node* newnode = new Node(item);
+                r= newnode;
+            }
+            else if (item <= r->data){
+                r->left= insert(r->left,item);
+            }
+            else{
+                r->right = insert(r->right,item);
+            }
+            return r;
+
+        }
+        void insert(int item){
+            root = insert(root,item);
+        }
+        
+        void Preorder(Node*r){
+            if(r==NULL){
+                return;
+            }
+            cout  << r->data <<"\t";
+            Preorder(r->left);
+            Preorder(r->right);
+        }
+
+        void inorder(Node*r){
+            if(r==NULL){
+                return;
+            }
+            inorder(r->left);
+            cout << r-> data <<"\t";
+            inorder(r->right);
+        }
+
+        void postorder(Node*r){
+            if(r==NULL){
+                return;
+            }
+            postorder(r->left);
+            postorder(r->right);
+            cout << r-> data<< "\t";
+        }
+    };
+
+int main(){
+
+}
